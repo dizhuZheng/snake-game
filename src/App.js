94afe107f60666b79e74e1bfca1b4d8663eefa9a
dizhuZheng <dizhu210@gmail.com>
2024-10-17 12:40:56 -0400

    canvas can work now

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ export default function App() {
       <ChakraProvider>
         <Container maxW="container.lg" centerContent>
           <Heading as="h1" size="xl">SNAKE GAME</Heading>
+          <Heading as='h4' size='md'>Current Score</Heading>
           <Board height={600} width={1000} />
+        </Container>
+        <Container centerContent>
+        <Heading as='h4' size='md'>How to Play</Heading>
+        <Heading as='h5' size='sm'>Note: Start the game by pressing </Heading>
         </Container>
       </ChakraProvider>
     </Provider>
   );
 }
-
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useRef, useEffect }  from "react"
 
 export interface IBoard {
     height: number;
     width: number;
   }
-  
-const Board = ({ height, width }: IBoard) => {
+
+const Board = ({ height, width}: IBoard) => {
+    const canvasRef = useRef(null)
+      
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        //Our first draw
+        context.fillStyle = 'purple'
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+      }, [])
     return (
         <canvas
+        ref={canvasRef}
         style = {{
             border: "3px solid black",
         }}
